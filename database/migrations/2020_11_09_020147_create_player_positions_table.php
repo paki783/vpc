9e0dea25985ec14a86a34b5a4ffc58299ae2499f
Ali Haider <ali.haider@invisionsolutions.ca>
2021-05-07 02:10:13 +0500

    New migration of player position
    (cherry picked from commit aa79bfdb588f3acb407c16ada812763a8802edac)

--- database/migrations/2020_11_09_020147_create_player_positions_table.php
+++ database/migrations/2020_11_09_020147_create_player_positions_table.php
@@ -16,16 +16,10 @@ class CreatePlayerPositionsTable extends Migration
         Schema::create('player_positions', function (Blueprint $table) {
             $table->engine = 'InnoDB';
             $table->bigIncrements('id')->unsigned();
-            $table->unsignedBigInteger('team_id');
+            $table->unsignedBigInteger('line_up_id');
             $table->unsignedBigInteger('user_id');
             $table->unsignedBigInteger('position_id');
-            $table->unsignedBigInteger('vpc_system_id');
             $table->timestamps();
-
-            $table->foreign('team_id')->references('id')->on('teams')->onDelete('cascade');
-            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
-            $table->foreign('position_id')->references('id')->on('positions')->onDelete('cascade');
-            $table->foreign('vpc_system_id')->references('id')->on('vpcsystems')->onDelete('cascade');
         });
     }
 
